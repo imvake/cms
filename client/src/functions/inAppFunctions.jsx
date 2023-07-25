071a65c22f9f596691849dc283fb97d100173c5f
imvake <104240205+imvake@users.noreply.github.com>
2023-07-25 14:59:03 +0400

    Update inAppFunctions.jsx

--- client/src/functions/inAppFunctions.jsx
+++ client/src/functions/inAppFunctions.jsx
@@ -3,7 +3,7 @@ import Swal from "sweetalert2";
 
 const fetchApi = async (url, setState, resetState) => {
   try {
-    const response = await axios.get(`http://localhost:3001/${url}`);
+    const response = await axios.get(`https://deploy-cms-api.vercel.app/${url}`);
     setState(response.data);
   } catch (error) {
     Swal.fire({
